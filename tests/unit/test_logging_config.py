258d04b8f83760cb88913835b8779eaeda96c7ca
RBrouq <brouquier57@gmail.com>
2025-07-21 20:10:11 +0200

    cleaning logs

--- tests/unit/test_logging_config.py
+++ tests/unit/test_logging_config.py
@@ -1,12 +1,12 @@
 # tests/unit/test_logging_config.py
 from loguru import logger
 
-from trading_framework.logging_config import configure_logging
+from trading_framework.logging_config import configure
 
 
 def test_configure_logging(tmp_path, capsys) -> None:
     log_file = tmp_path / "app.log"
-    configure_logging(str(log_file))
+    configure(str(log_file))
     logger.debug("debug-msg")
     logger.info("info-msg")
     # on vérifie que le fichier a été créé et contient "debug-msg"
